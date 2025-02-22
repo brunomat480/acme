51a5c006182783d90f13aa5c84d2bc41564a39b1
Bruno José <bru.matias100@gmail.com>
2025-02-22 17:56:26 -0300

    feat: making parallel request

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,9 +5,20 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+  const [
+    revenue,
+    latestInvoices,
+    {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices
+    },
+  ] = await Promise.all([
+    fetchRevenue(),
+    fetchLatestInvoices(),
+    fetchCardData(),
+  ]);
 
   return (
     <main>
